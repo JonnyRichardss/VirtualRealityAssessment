e3a2d3b081b05c3c714f78a2850ec427a2725e3a
Jonny <jonnyrichards254@gmail.com>
2024-09-27 13:30:22 +0100

    Added car and traffic lights

---
+++ Assets/CarRoute.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRoute : MonoBehaviour
+{
+
+    private int targetWP = 0;
+    public GameObject WaypointContainer;
+    private List<Transform> wps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!WaypointContainer) Destroy(gameObject);//if we dont have waypoint container die
+        wps = new List<Transform>();
+        foreach (Transform t in WaypointContainer.GetComponentsInChildren<Transform>())
+        {
+            if (ReferenceEquals(WaypointContainer.transform, t)) continue; //ignore the container itself
+            wps.Add(t);
+        }
+        if (wps.Count == 0) Destroy(gameObject); //if container has no children die
+        SetRoute();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 displacement = wps[targetWP].position - transform.position;
+        displacement.y = 0;
+        float dist = displacement.magnitude;
+        if (dist < 0.1f)
+        {
+            SetRoute();
+
+        }
+        //calculate velocity for this frame
+        Vector3 velocity = displacement;
+        velocity.Normalize();
+        velocity *= 2.5f;
+        //apply velocity
+        Vector3 newPosition = transform.position;
+        newPosition += velocity * Time.deltaTime;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.MovePosition(newPosition);
+        //align to velocity
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, velocity,
+        10.0f * Time.deltaTime, 0f);
+        Quaternion rotation = Quaternion.LookRotation(desiredForward);
+        rb.MoveRotation(rotation);
+    }
+    void SetRoute()
+    {
+        targetWP++;
+        targetWP %= wps.Count;
+    }
+}
